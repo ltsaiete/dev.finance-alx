3fc24f06b4baf7f8354031c9a1f66b14e7ec224d
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-12-13 11:30:41 +0200

    Link User controller methods to specific user routes

--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance, FastifyRegisterOptions, FastifyReply, FastifyRequest } from "fastify";
-import UserController from "../Controller/UserController";
+import UserController from "../controllers/UserController";
 
 async function userRoutes(fastify: FastifyInstance, options: any) {
     fastify.get('/:id', UserController.getUser);
