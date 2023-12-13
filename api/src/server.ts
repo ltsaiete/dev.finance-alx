1fdef6b7636a8a9bd7741bb783cd48986be338ac
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-12-13 11:51:27 +0200

    Fix eslint errors

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,11 @@
 import Fastify from 'fastify';
 import routes from './routes';
-import userRoutes from './routes/user';
+
 const server = Fastify({
 	logger: true
 });
 
 // register routes
-server.register(routes, { prefix: "/api/v1"});
+server.register(routes, { prefix: '/api/v1' });
 
-server.listen({ port: 3333 }).then(() => console.log('Server running on 3333'));
+server.listen({ port: 3333 }).then(() => { console.log('Server running on 3333'); });
