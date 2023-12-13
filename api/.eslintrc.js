183bb9f4679931f1b592fbb55daeed6767fd34bb
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-12-13 11:51:10 +0200

    Add eslint configuration

---
+++ api/.eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'overrides': [
+        {
+            'env': {
+                'node': true
+            },
+            'files': [
+                '.eslintrc.{js,cjs}'
+            ],
+            'parserOptions': {
+                'sourceType': 'script'
+            }
+        }
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            'tab'
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'max-classes-per-file': 'off',
+        'no-underscore-dangle': 'off',
+        'no-console': 'off',
+        'no-shadow': 'off',
+        'no-restricted-syntax': [
+            'error',
+            'LabeledStatement',
+            'WithStatement',
+        ],
+    }
+};
